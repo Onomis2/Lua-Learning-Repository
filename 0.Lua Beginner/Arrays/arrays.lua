5491545605bc51b9dfa69d9a93998c24c18ff2a0
Simon <onomis2@outlook.com>
2024-03-27 14:13:18 +0100

    Learned about arrays

---
+++ 0.Lua Beginner/Arrays/arrays.lua
@@ -0,0 +1,8 @@
+number = {}
+print("Enter number 1")
+number[1] = io.read()
+print("Enter number 2")
+number[2] = io.read()
+print("Enter number 3")
+number[3] = io.read()
+print(string.format("You have entered 3 numbers and they are: %d, %d and %d", number[1], number[2], number[3]))
