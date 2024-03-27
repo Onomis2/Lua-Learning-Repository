a04682fca9907675c04b1d00a530dcfc999209c1
Simon <onomis2@outlook.com>
2024-03-27 14:32:52 +0100

    Learned operators

---
+++ 0.Lua Beginner/Arrays/calculator.lua
@@ -0,0 +1,7 @@
+number = {}
+print("Enter number 1")
+number[1] = io.read()
+print("Enter number 2")
+number[2] = io.read()
+result = number[1] + number[2]
+print(string.format("The result is: %d", result))
